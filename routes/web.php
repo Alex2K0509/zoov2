b4fe57a2cbf003d8ea0dd1fc20c1f61bfea85b7c
Alejandro Yosmar Landaverde Vergara <alexvergara1998@gmail.com>
2021-11-14 23:48:36 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,8 @@ Route::post('/notification/post', 'App\Http\Controllers\Records\RecordsControlle
 Auth::routes(["register" => false]);
 Route::group(['middleware' => ['auth','userable']], function () {
 
-    Route::get('/home', 'App\Http\Controllers\Records\RecordsController@tableeventos')->name('home');
-    #Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
-    Route::get('/home', 'App\Http\Controllers\Records\RecordsController@tableeventos')->name('home');
+    Route::get('/home', 'App\Http\Controllers\RECORDS\RecordsController@tableeventos')->name('home');
+    Route::get('/home', 'App\Http\Controllers\RECORDS\RecordsController@tableeventos')->name('home');
 
     Route::get('user', 'App\Http\Controllers\UserController@index')->name('user.index'); #pagina inicial
     Route::get('admin', 'App\Http\Controllers\UserController@admin')->name('user.admin'); #pagina inicial
@@ -54,33 +53,33 @@ Route::group(['middleware' => ['auth','userable']], function () {
     })->name('table');
 
     #ruta para insertar eventos
-    Route::post('/evento/add', 'App\Http\Controllers\Catalogos\CatalogosController@InserEvent')->name('eventoInsert');
-    Route::post('/animal/add', 'App\Http\Controllers\Catalogos\CatalogosController@InsertAnimal')->name('InsertAnimal');
+    Route::post('/evento/add', 'App\Http\Controllers\CATALOGOS\CatalogosController@InserEvent')->name('eventoInsert');
+    Route::post('/animal/add', 'App\Http\Controllers\CATALOGOS\CatalogosController@InsertAnimal')->name('InsertAnimal');
     #ruta para editar un evento
     #Route::get('/edit/evento', 'App\Http\Controllers\CATALOGOS\RecordsController@editEventos')->name('edit.eventos');
 
     #rutas para formar los data tables
-    Route::get('/table/eventos', 'App\Http\Controllers\Records\RecordsController@tableeventos')->name('table.eventos');
-    Route::get('/table/pubs', 'App\Http\Controllers\Records\RecordsController@tablepublicaciones')->name('table.pubs');
-    Route::get('/table/animals', 'App\Http\Controllers\Records\RecordsController@tableAnimales')->name('table.animals');
+    Route::get('/table/eventos', 'App\Http\Controllers\RECORDS\RecordsController@tableeventos')->name('table.eventos');
+    Route::get('/table/pubs', 'App\Http\Controllers\RECORDS\RecordsController@tablepublicaciones')->name('table.pubs');
+    Route::get('/table/animals', 'App\Http\Controllers\RECORDS\RecordsController@tableAnimales')->name('table.animals');
     #rutas pára los visualizar la información del evento o publicación desde el modal de editar
-    Route::get('/info/eventos', 'App\Http\Controllers\Records\RecordsController@infoediteve')->name('info.eventos');
-    Route::get('/info/publicaciones', 'App\Http\Controllers\Records\RecordsController@infoPublications')->name('info.publicaciones');
-    Route::get('/info/animals', 'App\Http\Controllers\Records\RecordsController@infoAnimals')->name('info.animals');
+    Route::get('/info/eventos', 'App\Http\Controllers\RECORDS\RecordsController@infoediteve')->name('info.eventos');
+    Route::get('/info/publicaciones', 'App\Http\Controllers\RECORDS\RecordsController@infoPublications')->name('info.publicaciones');
+    Route::get('/info/animals', 'App\Http\Controllers\RECORDS\RecordsController@infoAnimals')->name('info.animals');
     #rutas para editar un evento o publicación desde el modal de editar
-    Route::post('/edit/eventos', 'App\Http\Controllers\Records\RecordsController@editeventos')->name('edit.eventos');
-    Route::post('/edit/pubs', 'App\Http\Controllers\Records\RecordsController@editpubs')->name('edit.pubs');
-    Route::post('/edit/animals', 'App\Http\Controllers\Records\RecordsController@editanimals')->name('edit.animals');
+    Route::post('/edit/eventos', 'App\Http\Controllers\RECORDS\RecordsController@editeventos')->name('edit.eventos');
+    Route::post('/edit/pubs', 'App\Http\Controllers\RECORDS\RecordsController@editpubs')->name('edit.pubs');
+    Route::post('/edit/animals', 'App\Http\Controllers\RECORDS\RecordsController@editanimals')->name('edit.animals');
     #rutas para eliminar un evento del datatable de eventos
-    Route::delete('delete/evento', 'App\Http\Controllers\Records\RecordsController@deleteEvento')->name('delete.eventos');
-    Route::delete('delete/pub', 'App\Http\Controllers\Records\RecordsController@deletePub')->name('delete.pub');
-    Route::delete('delete/animals', 'App\Http\Controllers\Records\RecordsController@deleteAnimals')->name('delete.animals');
+    Route::delete('delete/evento', 'App\Http\Controllers\RECORDS\RecordsController@deleteEvento')->name('delete.eventos');
+    Route::delete('delete/pub', 'App\Http\Controllers\RECORDS\RecordsController@deletePub')->name('delete.pub');
+    Route::delete('delete/animals', 'App\Http\Controllers\RECORDS\RecordsController@deleteAnimals')->name('delete.animals');
     #RUTAS PARA EL SELEC2 HE INSERCIÓN DE POST
-    Route::post('/all/animals', 'App\Http\Controllers\Records\RecordsController@getAnimals')->name('all.animals');
-    Route::post('/insert/post', 'App\Http\Controllers\Publicaciones\PublicacionesController@inserPost')->name('insert.post');
+    Route::post('/all/animals', 'App\Http\Controllers\RECORDS\RecordsController@getAnimals')->name('all.animals');
+    Route::post('/insert/post', 'App\Http\Controllers\PUBLICACIONES\PublicacionesController@inserPost')->name('insert.post');
     #RUTAS PARA ENVIAR NOTIFICACIONES DE LOS EVENTOS Y PUBLICACIONES
-    Route::post('/notification/eventos', 'App\Http\Controllers\Records\RecordsController@sentNofitication')->name('noti.eventos');
-    Route::post('/notification/post', 'App\Http\Controllers\Records\RecordsController@sentNofiticationPost')->name('noti.posts');
+    Route::post('/notification/eventos', 'App\Http\Controllers\RECORDS\RecordsController@sentNofitication')->name('noti.eventos');
+    Route::post('/notification/post', 'App\Http\Controllers\RECORDS\RecordsController@sentNofiticationPost')->name('noti.posts');
     #RUTAS PARA EDITAR LA IMAGEN DE PERFIL
     Route::post('/profile/edit/pic', 'App\Http\Controllers\ProfileController@editPic')->name('edit.pic');
 
