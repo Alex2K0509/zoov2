5e952da14321580ba7c782a4a911127ead0d6efa
Alejandro Yosmar Landaverde Vergara <alexvergara1998@gmail.com>
2021-11-29 19:59:05 -0500

    footer

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -13,15 +13,6 @@
             <li class="nav-item">
                 <a style="color: black"href="https://www.updivision.com" class="nav-link" target="_blank">Updivision</a>
             </li>
-            <li class="nav-item">
-                <a style="color: black" href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: black" href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a style="color: black" href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-            </li>
         </ul>
     </div>
 </div>
